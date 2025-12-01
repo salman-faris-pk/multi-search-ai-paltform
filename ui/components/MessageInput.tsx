bf514d8abfe8f4abd9edf98f34b4bc57f79ae244
salmanul faris pk <salmanulfarispk2001@gmail.com>
2025-12-01 22:14:59 +0530

    messageinput compo completes...

--- ui/components/MessageInput.tsx
+++ ui/components/MessageInput.tsx
@@ -1,5 +1,8 @@
-import { useState } from "react";
-
+import { ArrowUp } from "lucide-react";
+import React, { useState } from "react";
+import { Attach, CopilotToggle } from "./MessageInputActions";
+import TextareaAutosize from "react-textarea-autosize";
+import { cn } from "@/lib/utils";
 
 
 interface MessageInputProps{
@@ -9,14 +12,81 @@ interface MessageInputProps{
 
 const MessageInput = ({sendMessage,loading}:MessageInputProps) => {
 
-     const [copilotEnabled, setCopilotEnabled] = useState(false);
+  const [copilotEnabled, setCopilotEnabled] = useState(false);
   const [message, setMessage] = useState("");
   const [textareaRows, setTextareaRows] = useState(1);
 
-  const [mode, setMode] = useState<"multi" | "single">("single");
+
+
+  const handleSubmit =(e: React.FormEvent<HTMLFormElement>)=>{
+     if (loading) return;
+        e.preventDefault();
+        sendMessage(message);
+        setMessage("");
+  };
+
+  const handleKeydown=(e:React.KeyboardEvent<HTMLTextAreaElement>)=>{
+       if (e.key === "Enter" && !e.shiftKey && !loading) {
+            if (!message.trim()) return;
+          e.preventDefault();
+          sendMessage(message);
+          setMessage("");
+        }
+  };
+
+   const mode = textareaRows >= 2 && message.trim().length ? "multi" : "single";
 
   return (
-    <div>MessageInput</div>
+      <form
+      onSubmit={handleSubmit}
+      className={cn(
+        "bg-[#111111] p-4 flex items-center overflow-hidden border border-[#1C1C1C]",
+        mode === "multi" ? "flex-col rounded-lg" : "flex-row rounded-full"
+      )}
+    >
+      {mode === "single" && <Attach />}
+      <TextareaAutosize
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
+        onHeightChange={(height, props) => {
+          setTextareaRows(Math.ceil(height / props.rowHeight));
+        }}
+        onKeyDown={handleKeydown}
+        className="transition bg-transparent placeholder:text-white/50 placeholder:text-sm text-sm text-white resize-none focus:outline-none w-full max-h-24 lg:max-h-36 xl:max-h-48 grow shrink"
+        placeholder="Ask a follow up"
+      />
+      {mode === "single" && (
+        <div className="flex flex-row items-center space-x-4">
+          <CopilotToggle
+            copilotEnabled={copilotEnabled}
+            setCopilotEnabled={setCopilotEnabled}
+          />
+          <button
+            disabled={message.trim().length === 0 || loading}
+            className="bg-[#24A0ED] text-white disabled:text-white/50 hover:bg-opacity-85 transition duration-100 disabled:bg-[#ececec21] rounded-full p-2"
+          >
+            <ArrowUp size={17} />
+          </button>
+        </div>
+      )}
+      {mode === "multi" && (
+        <div className="flex flex-row items-center justify-between w-full pt-2">
+          <Attach />
+          <div className="flex flex-row items-center space-x-4">
+            <CopilotToggle
+              copilotEnabled={copilotEnabled}
+              setCopilotEnabled={setCopilotEnabled}
+            />
+            <button
+              disabled={message.trim().length === 0 || loading}
+              className="bg-[#24A0ED] text-white disabled:text-white/50 hover:bg-opacity-85 transition duration-100 disabled:bg-[#ececec21] rounded-full p-2"
+            >
+              <ArrowUp size={17} />
+            </button>
+          </div>
+        </div>
+      )}
+    </form>
   )
 }
 

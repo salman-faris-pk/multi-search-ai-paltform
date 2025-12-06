52302f7d7e9b10d034a5c9e7d529fed8b8473b52
salmanul faris pk <salmanulfarispk2001@gmail.com>
2025-12-06 22:25:57 +0530

    some changes..

--- src/lib/providers.ts
+++ src/lib/providers.ts
@@ -1,10 +1,10 @@
 import { ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
 import { getGeminaiApiKey,getOllamaApiEndpoint} from "../config.js"
-
-
+import { OllamaEmbeddings,ChatOllama } from "@langchain/ollama";
 
 
 export const getAvailableProviders = async() => {
+
 
     const geminaiApikey= getGeminaiApiKey();
     const ollamaEndpoint= getOllamaApiEndpoint();
@@ -48,13 +48,36 @@ export const getAvailableProviders = async() => {
     if(ollamaEndpoint){
         try {
             const response=await fetch(`${ollamaEndpoint}/api/tags`);
+            if (!response.ok) {
+            throw new Error(`Ollama API returned ${response.status}`);
+            }
 
-            const { models: ollamaModels}=await response.json();
-            
+
+            const { models: ollamaModels}= (await response.json()) as any
+
+            models["ollama"]= ollamaModels.reduce((acc, model) => {   // Reduce the array of Ollama models into an object like:
+                acc[model.model] = new ChatOllama({                   // { "modelName": ChatOllamaInstance }
+                    baseUrl: ollamaEndpoint,                          //This lets us dynamically support every installed Ollama model.
+                    model: model.model,
+                    temperature: 0.7
+                });
+
+                return acc;
+            },{});
+
+            if(Object.keys(models["ollama"]).length > 0 ){          //then create an OllamaEmbeddings client using the first model in the list.
+                models["ollama"]["embeddings"]= new OllamaEmbeddings({ 
+                    baseUrl: ollamaEndpoint,                              
+                    model: models["ollama"][Object.keys(models["ollama"])[0]].model, //then create an OllamaEmbeddings client using the first model in the list.
+                });
+            }
+
             
         } catch (error) {
             console.log(`Error loading Ollama models: ${error}`);
         }
     }
+
+    return models;
 
 };
--- src/websocket/connectionManager.ts
+++ src/websocket/connectionManager.ts
@@ -1,29 +1,15 @@
 import { WebSocket } from "ws"
 import { handleMessage } from "./messageHandler.js"
-import { ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
 import { getGeminaiApiKey } from "../config.js";
+import { getAvailableProviders } from "../lib/providers.js";
 
 
 
 export const handleConnection = async(ws:WebSocket)=>{
 
- const llm = new ChatGoogleGenerativeAI({
-  model: process.env.MODEL_NAME,
-  temperature: 0,
-  apiKey: getGeminaiApiKey(),
-});
+   const models=await getAvailableProviders();
+   
 
-// const Chatllm = new ChatGoogleGenerativeAI({
-//   model: process.env.CHAT_MODEL_NAME,
-//   temperature: 0.7,
-//   apiKey: getGeminaiApiKey(),
-// });
-
-
-const embeddings = new GoogleGenerativeAIEmbeddings({
-  model: process.env.EMBEDDINGS_MODEL,
-  apiKey: getGeminaiApiKey()
-});
 
     ws.on("message", async(message)=> await handleMessage(message.toString(), ws,llm,embeddings));
 
